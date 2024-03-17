502724d5b90cd2109dd3e5162878b127310fd000
jcurtis06 <06jcurtis@gmail.com>
2024-03-16 22:48:17 -0400

    added day night cycle

--- worlds/scripts/world.gd
+++ worlds/scripts/world.gd
@@ -2,3 +2,9 @@ extends Node2D
 class_name World
 
 @export var block_grid: BlockGrid
+
+@onready var monster: PackedScene = preload("res://entities/monster/monster.tscn")
+
+func _on_day_night_cycle_on_night_time() -> void:
+	print("night hath fallen")
+	add_child(monster.instantiate())
