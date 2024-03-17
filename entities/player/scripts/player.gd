4890e7877596ca8e41f5f8b310e27cf14b9eec77
jcurtis06 <06jcurtis@gmail.com>
2024-03-16 21:12:49 -0400

    added player animations

--- entities/player/scripts/player.gd
+++ entities/player/scripts/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 @onready var world: World = get_parent()
 @onready var attack := $AttackComponent
+@onready var animation := $Sprite
 
 func _physics_process(delta: float) -> void:
 	# MOVEMENT
@@ -21,3 +22,31 @@ func _physics_process(delta: float) -> void:
 	# BUILD
 	if Input.is_action_just_pressed("interact"):
 		world.block_grid.set_block_at(get_global_mouse_position(), test_block)
+	
+	# ANIMATIONS
+	_handle_animations()
+
+func _handle_animations() -> void:
+	if velocity.is_zero_approx():
+		return
+	
+	if velocity.y > 0:
+		animation.play("walk_down")
+		return
+	elif velocity.y < 0:
+		animation.play("walk_up")
+		return
+	
+	if velocity.x > 0:
+		animation.flip_h = false
+		animation.play("walk_right")
+	else:
+		animation.flip_h = true
+		animation.play("walk_right")
+	
+	#if velocity.y < 0:
+		#print("walking up")
+		#animation.play("walk_up")
+	#elif velocity.y > 0:
+		#print("walking down")
+		#animation.play("walk_down")
